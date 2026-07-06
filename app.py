dfb9a34a1ce36a99d8adc42d737076b19d4146aa
danems877-creator <danems877@gmail.com>
2026-07-06 21:32:04 +0200

    namen Update

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from pyzbar.pyzbar import decode as pyzbar_decode # Importiere die pyzbar-Biblio
 # --- Seitenkonfiguration ---
 # Dies sollte der erste Streamlit-Befehl im Skript sein.
 st.set_page_config(
-    page_title="Inventar-App",
+    page_title="Speisothek",
     page_icon="static/icons/icon-192x192.png", # Verwendet jetzt dein eigenes Icon
     layout="wide"
 )
